2704b4e01972574d3a2a1e28aebcfc41ac922b5b
John King <John@thumbtacksoftware.com>
2013-12-05 12:28:09 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StoneyPoint::Application.config.secret_key_base = '67f2302c8a0343e8e07554ea135116a3ab6eb94873f6bcd2a4c41a84bc0c9ea2043a79c2d6e702e881b499a911941b17c706c5465cb17698ce257f851c680e57'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StoneyPoint::Application.config.session_store :cookie_store, key: '_stoney_point_session'
