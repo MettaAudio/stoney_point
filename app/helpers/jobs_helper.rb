fc777c935a0c5beb82fb0f1a6c124e1832fc09ec
John King <John@thumbtacksoftware.com>
2014-01-02 12:01:10 -0500

    Fix error when Job doesn't have a committee

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,2 +1,10 @@
 module JobsHelper
+  def name_for(job)
+    html = ''
+    html << job.title
+    if job.committee
+      html <<  "<span class='muted'> - #{job.committee.name} committee"
+    end
+    html.html_safe
+  end
 end
