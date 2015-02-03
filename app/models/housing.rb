557e31dbdb19ff1e46781a760b16c24bd73572c4
John King <John@thumbtacksoftware.com>
2015-02-02 22:58:03 -0800

    Migrate all data over to the people table
    This commit updates all the tables to migrate over Caddies, Housing and
    Golfers. It does not verify that any of the front end works still,
    although the volunteer front end should still work. It also comments out
    most of the validation, etc. that is broken by the migration. Those will
    all be fixed in another branch that is merged after this one is deployed
    and migrated.

--- app/models/housing.rb
+++ app/models/housing.rb
@@ -1,12 +1,15 @@
 class Housing < ActiveRecord::Base
   belongs_to :volunteer
+  belongs_to :person
   has_and_belongs_to_many :golfers
   validates :available, presence: true
   validates :number_of_bedrooms, presence: true
   validates :number_of_bathrooms, presence: true
   validates :volunteer_id, presence: true
 
-  default_scope { includes(volunteer: :person).where('people.is_active = ?', true).order('people.last_name ASC') }
+  # default_scope { includes(:person).where('people.is_active = ?', true).order('people.last_name ASC') }
+  # default_scope { includes(volunteer: :person).where('people.is_active = ?', true).order('people.last_name ASC') }
+  # default_scope { includes(:volunteer).order('volunteers.last_name ASC') }
 
   def self.total_number_of_beds
     count = 0
