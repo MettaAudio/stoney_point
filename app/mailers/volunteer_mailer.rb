af685c5d0578bb4fcb531e8ec951bf8f9dc1adcc
John King <John@thumbtacksoftware.com>
2016-01-12 07:54:09 -0800

    Add SendGrid

--- app/mailers/volunteer_mailer.rb
+++ app/mailers/volunteer_mailer.rb
@@ -4,6 +4,6 @@ class VolunteerMailer < ActionMailer::Base
   def not_found(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
-    mail(to: 'john@mettaaudio.com', subject: "User not found: #{first_name} #{last_name}")
+    mail(to: 'john@mettaaudio.com', cc: 'ctomking@gmail.com', subject: "User not found: #{first_name} #{last_name}")
   end
 end
