6bbe207dc23f6c9a679e58fda2d1dbab9400beff
John King <John@thumbtacksoftware.com>
2016-01-12 08:13:10 -0800

    Update email addresses

--- app/mailers/volunteer_mailer.rb
+++ app/mailers/volunteer_mailer.rb
@@ -1,9 +1,9 @@
 class VolunteerMailer < ActionMailer::Base
-  default from: "stoneypointvolunteer@gmail.com"
+  default from: "stoneypointhousing@gmail.com"
 
   def not_found(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
-    mail(to: 'john@mettaaudio.com', cc: 'ctomking@gmail.com', subject: "User not found: #{first_name} #{last_name}")
+    mail(to: 'stoneypointvolunteer@gmail.com', bcc: 'john@mettaaudio.com', subject: "User not found: #{first_name} #{last_name}")
   end
 end
