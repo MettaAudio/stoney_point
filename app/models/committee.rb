44861a548d36f86df90c826c1e028ca49b6d95dd
John King <John@thumbtacksoftware.com>
2017-03-13 08:29:57 -0700

    Exclude select committees from Schedule

--- app/models/committee.rb
+++ app/models/committee.rb
@@ -3,6 +3,7 @@ class Committee < ActiveRecord::Base
   validates :name, presence: true
 
   JUNIOR_CLINIC = "Junior Clinic"
+  EXCLUDED_FROM_SCHEDULING = ['ProShop', JUNIOR_CLINIC]
 
   scope :scheduleable, -> {where.not(name: [PersonForm::DEFAULT_COMMITTEE_NAME,'Operations'])}
   scope :sorted, -> { order('name ASC') }
